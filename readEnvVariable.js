862c3309e117a70bfc110347624a2553fd41176b
SubodhJondhale <122435965+SubodhJondhale@users.noreply.github.com>
2023-01-20 20:05:30 +0530

    Add files via upload

---
+++ readEnvVariable.js
@@ -0,0 +1,3 @@
+const name = process.env.USERNAME;
+console.log(`Hello ${name}`);
+//your process.env is a object now you want to set new key Username for that use command "$env:Username=Subodh" this will set a new key Username in process.env.object  and its value wil be Subodh
